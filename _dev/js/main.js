57b4570d33fac93ebfaad4eeec707fca9adfed77
Eugen <evgeniy.sayenko@gmail.com>
2017-04-24 00:45:10 +0300

    FullPageMenu настроено

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -5,11 +5,31 @@ $(document).ready(function () {
             var loadedSection = $(this);
 
             //using index
-            if (index == 3) {
-                alert("Section 3 ended loading");
+            if (index == 1) {
+                $(".works__link").removeClass("works__link-white");
+                $(".contact__link").removeClass("contact__link-black");
             }
 
-            //using anchorLink
+
+            if (index == 2) {
+                $(".works__link").addClass("works__link-white");
+                $(".contact__link").addClass("contact__link-black");
+            }
+
+            if (index == 3) {
+                $(".works__link").removeClass("works__link-white");
+                $(".contact__link").removeClass("contact__link-black");
+            }
+
+            if (index == 4) {
+                $(".works__link").addClass("works__link-white");
+                $(".contact__link").addClass("contact__link-black");
+            }
+
+            if (index == 5) {
+                $(".works__link").removeClass("works__link-white");
+                $(".contact__link").removeClass("contact__link-black");
+            }
 
         }
     });
