a0631468cd12004fcf44fb0de0f655007ff0a51a
Eugen <evgeniy.sayenko@gmail.com>
2017-04-28 00:12:03 +0300

    Главная десктоп закончена со скриптами

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 $(document).ready(function () {
     $('#fullpage').fullpage({
 
@@ -34,3 +36,20 @@ $(document).ready(function () {
         }
     });
 });
+
+
+var openNav = document.querySelectorAll(".cmn-toggle-switch");
+var mainNav = document.querySelector(".main__nav");
+
+for (var i = 0; i < openNav.length; i++) {
+
+    openNav[i].addEventListener("click", function (event) {
+        event.preventDefault();
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("menu__popup-open");
+        mainNav.classList.toggle("main__nav-close");
+
+    })
+
+}
+
